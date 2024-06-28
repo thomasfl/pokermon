09543a1435d10ee4c06aeacf5738b1011e75d0cb
Thomas Flemming <thomas.flemming@gmail.com>
2024-06-28 10:35:10 +0200

    add readme

--- start_docker.sh
+++ start_docker.sh
@@ -2,4 +2,9 @@
 cd server
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
-docker compose up --build --force-recreate 
+docker compose up --build --force-recreate &
+
+cd ../client
+docker build . -t "pokermon-client:v1.0"
+docker run -p 3000:3000 pokermon-client:v1.0 &
+
